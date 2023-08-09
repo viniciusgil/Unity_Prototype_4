104340121a7644c25f46cfb515495d5b6c717a65
viniciusgil <viniciusgil_es@hotmail.com>
2023-08-09 16:42:37 +0100

    Adding powerup

--- Prototype 4/Assets/Script/Enemy.cs
+++ Prototype 4/Assets/Script/Enemy.cs
@@ -7,6 +7,9 @@ public class Enemy : MonoBehaviour
     public float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
+
+    public int Length { get; internal set; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,12 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
-      enemyRb.AddForce( lookDirection * speed);  
+        enemyRb.AddForce( lookDirection * speed);  
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
